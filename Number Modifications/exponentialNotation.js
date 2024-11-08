5c2a1727376ee1b2d944861da6ea0ce63b1db487
Tasuku-CV6 <Tasukuseikatsu45@gmail.com>
2024-11-07 19:02:26 -0700

    Changed one last thing in the file Exponential Notation file

--- Number Modifications/exponentialNotation.js
+++ Number Modifications/exponentialNotation.js
@@ -7,4 +7,4 @@ function expo(x, f) {
   return Number.parseFloat(x).toExponential(f);
 }
 console.log("After: ", expo(123456, 2));
-// output: Should display a Exponential Notation of "After: 1.23e+5"
+// output: Displays a Exponential Notation of "After: 1.23e+5"
